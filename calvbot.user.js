435479fe8386bc564dfcd5a353c6563e44b255b9
CalvT <calvin@jstomkins.com>
2017-03-21 09:17:57 -0300

    Update to readable vars

--- calvbot.user.js
+++ calvbot.user.js
@@ -1,42 +1,44 @@
 // ==UserScript==
 // @name        Smokey Predictor
 // @namespace   smokeypredictor
-// @version      0.2
+// @version      0.3
 // @grant        none
 // ==/UserScript==
 (function () {
   'use strict';
+  var keywords = [
+    'blacklisted website',
+    'care',
+    'cream',
+    'enhancement',
+    'facial',
+    'http',
+    'male',
+    'muscle',
+    'skin',
+    'supplement'
+  ];
+  var answertp = '@CalvT I\'m thinking tp? - Weight: ';
+  var answerv;
   function CalvBotInterval() {
     $('.CalvBot').removeClass('CalvBot');
     $('#chat .user-container:last-child.user-64521').addClass('CalvBot');
     $('#chat .user-container:last-child.user-120914').addClass('CalvBot');
-    var ab1 = $('.CalvBot .messages .message:last-child .content').text();
-    var ab2 = ab1.toLowerCase();
-    var ba1 = [
-      'male',
-      'enhancement',
-      'skin',
-      'care',
-      'facial',
-      'cream',
-      'muscle',
-      'supplement'
-    ];
-    var ab3 = '@CalvT I\'m thinking tp? - Weight: ';
-    var ab4;
+    var smokey = $('.CalvBot .messages .message:last-child .content').text();
+    var smokeylc = smokey.toLowerCase();
     function contains(target, pattern) {
       var value = 0;
       pattern.forEach(function (word) {
         value = value + target.includes(word);
       });
-      ab4 = value;
-      return (value > 0);
+      answerv = value;
+      return (value >= 2);
     }
-    if (contains(ab2, ba1)) {
-      console.log(ab4 + ab1);
-      var ab5 = ab4 * 50;
-      var ab6 = ab3 + ab5;
-      document.getElementById('input').value = ab6;
+    if (contains(smokeylc, keywords)) {
+      console.log(answerv + smokey);
+      var answerw = answerv * 50;
+      var answero = answertp + answerw;
+      document.getElementById('input').value = answero;
       $('#sayit-button').click();
     } else {
       //false statement..do nothing
