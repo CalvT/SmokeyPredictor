586459e259c616c0b3618be9a02d05c9cf000811
CalvT <calvin@jstomkins.com>
2017-03-21 14:11:18 -0300

    Added Commands

--- calvbot.user.js
+++ calvbot.user.js
@@ -1,25 +1,32 @@
 // ==UserScript==
 // @name        Smokey Predictor
 // @namespace   smokeypredictor
-// @version      0.4
+// @version      0.4.1
 // @grant        none
 // ==/UserScript==
 (function () {
   'use strict';
+  document.getElementById('input').value = '@CalvT hey \o';
+  $('#sayit-button').click();
   var keywords = [
     'blacklisted website',
     'care',
     'cream',
     'enhancement',
     'facial',
+    'food',
     'http',
     'male',
+    'miracle',
     'muscle',
     'skin',
     'supplement'
   ];
   var answertp = '@CalvT I\'m thinking tp? - Weight: ';
   var answerv;
+  var alive = '@calvbot alive?';
+  var location = '@calvbot location?';
+  var restart = '@calvbot restart please';
   function CalvBotInterval() {
     $('.CalvBot').removeClass('CalvBot');
     $('.CalvBotCommand').removeClass('CalvBotCommand');
@@ -46,9 +53,24 @@
     } else {
       //false statement..do nothing
     }
-    if (cbcommandlc === '@calvbot alive?') {
+    if (cbcommandlc === alive) {
       document.getElementById('input').value = '@CalvT as much as you are :)';
       $('#sayit-button').click();
+    } else {
+      //false statement..do nothing
+    }
+
+    if (cbcommandlc === location) {
+      document.getElementById('input').value = '@CalvT your AWS EC2 instance';
+      $('#sayit-button').click();
+    } else {
+      //false statement..do nothing
+    }
+
+    if (cbcommandlc === restart) {
+      document.getElementById('input').value = '@CalvT ok, give me a minute';
+      $('#sayit-button').click();
+      location.reload(true);
     } else {
       //false statement..do nothing
     }
