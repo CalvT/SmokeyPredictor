23237b3c24d16fdbd21ba593ff66567648c1c93c
CalvT <calvin@jstomkins.com>
2017-03-21 21:29:04 -0300

    Better readability, more keywords, increased value to 3

--- calvbot.user.js
+++ calvbot.user.js
@@ -1,36 +1,42 @@
 // ==UserScript==
 // @name        Smokey Predictor
 // @namespace   smokeypredictor
-// @version      0.4.8
+// @version      0.5
 // @grant        none
 // ==/UserScript==
 (function () {
   'use strict';
+
+  // Startup Function
   window.setTimeout(slowStart, 3000);
   function slowStart() {
     document.getElementById('input').value = '@CalvT hey o/';
     $('#sayit-button').click();
   }
+
+  // Trigger Keywords
   var keywords = [
+    'apple.se',
+    'askubuntu.com',
+    'drupal.se',
+    'graphicdesign.se',
     'blacklisted website',
     'care',
     'cream',
     'enhancement',
+    'enlargement',
     'facial',
     'food',
     'http',
     'male',
     'miracle',
     'muscle',
+    'penis',
     'skin',
     'supplement'
   ];
-  var answertp = '@CalvT I\'m thinking tp? - Weight: ';
-  var answerv;
-  var alive = '@calvbot alive?';
-  var location = '@calvbot location?';
-  var restart = '@calvbot restart please';
-  var reload = function() {window.location.href=window.location.href};
+
+  // Reading Chat Function
   function CalvBotInterval() {
     $('.CalvBot').removeClass('CalvBot');
     $('.CalvBotCommand').removeClass('CalvBotCommand');
@@ -40,13 +46,17 @@
     var smokeylc = smokey.toLowerCase();
     var cbcommand = $('.CalvBotCommand .messages .message:last-child .content').text();
     var cbcommandlc = cbcommand.toLowerCase();
+
+    // Answer Smokey Function
+    var answertp = '@CalvT I\'m thinking tp? - Weight: ';
+    var answerv;
     function contains(target, pattern) {
       var value = 0;
       pattern.forEach(function (word) {
         value = value + target.includes(word);
       });
       answerv = value;
-      return (value >= 2);
+      return (value >= 3);
     }
     if (contains(smokeylc, keywords)) {
       console.log(answerv + smokey);
@@ -57,22 +67,32 @@
     } else {
       //false statement..do nothing
     }
+
+    // Command Functions
+    var alive = '@calvbot alive?';
+    var alivereply = '@CalvT as much as you are :)',
+    var location = '@calvbot location?';
+    var locationreply = '@CalvT your AWS EC2 instance',
+    var restart = '@calvbot restart please';
+    var restartreply ='@CalvT ok, give me a minute',
+    var reload = function() {window.location.href=window.location.href};
+
     if (cbcommandlc === alive) {
-      document.getElementById('input').value = '@CalvT as much as you are :)';
+      document.getElementById('input').value = alivereply;
       $('#sayit-button').click();
     } else {
       //false statement..do nothing
     }
 
     if (cbcommandlc === location) {
-      document.getElementById('input').value = '@CalvT your AWS EC2 instance';
+      document.getElementById('input').value = locationreply;
       $('#sayit-button').click();
     } else {
       //false statement..do nothing
     }
 
     if (cbcommandlc === restart) {
-      document.getElementById('input').value = '@CalvT ok, give me a minute';
+      document.getElementById('input').value = restartreply;
       $('#sayit-button').click();
       window.setTimeout(reload, 3000);
     } else {
