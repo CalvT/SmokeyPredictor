199e4b31b8cf8bc965f39c6d4626b98ed7804a32
CalvT <calvin@jstomkins.com>
2017-03-30 06:37:00 -0300

    Moved lastpostext out of function

--- drupal.user.js
+++ drupal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Drupal Predictor Sandboxed
 // @namespace   calvtdrupalpredictorsandboxed
-// @version     0.3
+// @version     0.3.1
 // @match        http://*/*
 // @downloadURL 
 // @updateURL 
@@ -11,13 +11,15 @@
   'use strict';
 
   // Version
-  var cbversion = '0.3';
+  var cbversion = '0.3.1';
 
   // Startup function
   window.setTimeout(slowStart, 3000);
   function slowStart() {
       console.log('@CalvT Hey o/')
   }
+
+  var lastposttext;
 
   // Chat Reading Function
   function CalvDrupalBot () {
@@ -27,7 +29,6 @@
       var smokey = $('.DrupalBot .messages .message:last-child .content').text();
       var smokeylc = smokey.toLowerCase();
       var posttext = $('.DrupalBot .messages .message:last-child .content a:nth-child(3)').attr('href');
-      var lastposttext;
 
       // Targetted Site
       var site = 'drupal.se';
