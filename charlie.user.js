53c62767f5bafb68e29b282b399e0565fccabef9
CalvT <calvin@jstomkins.com>
2017-04-27 08:15:26 -0300

    Separation of users

--- charlie.user.js
+++ charlie.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Charlie Sandboxed
 // @namespace   charliesandboxed
-// @version     0.2.1
+// @version     0.2.2
 // @match       *://chat.stackexchange.com/*
 // @downloadURL 
 // @updateURL 
@@ -9,7 +9,7 @@
 // ==/UserScript==
 
 // Version
-var cbversion = '0.2.1';
+var cbversion = '0.2.2';
 
 // Startup
 window.setTimeout(cbstartup, 3000);
@@ -20,23 +20,35 @@ function cbstartup() {
 
 // 'Out of Function'
 var lastpost;
+var commandusers = '.user-64521';
+var smokeyusers = '.user-120914, .user-272039';
 
 function CharlieBot() {
     $('.Charlie').removeClass('Charlie');
     $('#chat .user-container:last-child').addClass('Charlie');
+    if ($('.Charlie').is(commandusers)) {
+        Commands();
+    } else if ($('.Charlie').is(smokeyusers)) {
+        SmokeyReply();
+    }
+
+}
+
+function SmokeyReply() {
     var smokey = $('.Charlie .messages .message:last-child .content').text();
     var smokeylc = smokey.toLowerCase();
     if (smokeylc) {
         if (smokeylc !== lastpost) {
             console.log(smokeylc);
             lastpost = smokeylc;
-        } else {
-            // do nothing
         }
-    } else {
-        // do nothing
     }
+}
 
+function Commands() {
+    var chatcommand = $('.Charlie .messages .message:last-child .content').text();
+    var chatcommandlc = chatcommand.toLowerCase();
+    console.log("Command: " + chatcommandlc);
 }
 
 var refreshID = setInterval(CharlieBot, 2000);
